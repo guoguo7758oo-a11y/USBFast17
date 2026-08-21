9a687aabdf6fa4611873296a4078cfe1341be997
guoguo7758oo-a11y <guoguo7758oo@gmail.com>
2026-08-21 22:43:34 +0800

    Update USBFast17Prefs.m

--- USBFast17Prefs/USBFast17Prefs.m
+++ USBFast17Prefs/USBFast17Prefs.m
@@ -1,1 +1,21 @@
+#import <UIKit/UIKit.h>
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
 
+@interface USBFast17PrefsListController : PSListController
+@end
+
+@implementation USBFast17PrefsListController
+
+- (NSArray *)specifiers {
+    if (!_specifiers)
+        _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+    return _specifiers;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"USBFast17";
+}
+
+@end
